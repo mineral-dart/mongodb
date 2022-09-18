06f75bdd3bf4a832bc461f36f51524139728774c
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-09-19 00:28:31 +0200

    feat: Initial commit

---
+++ bin/mineral_mongodb.dart
@@ -0,0 +1,5 @@
+import 'package:mineral_mongodb/mineral_mongodb.dart' as mineral_mongodb;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mineral_mongodb.calculate()}!');
+}
---
+++ test/mineral_mongodb_test.dart
@@ -0,0 +1,8 @@
+import 'package:mineral_mongodb/mineral_mongodb.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
