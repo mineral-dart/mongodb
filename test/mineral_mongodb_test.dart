dfe2de8f134ee75b1f6fca2ce9d8bb7d8c61784b
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-09-19 18:00:44 +0200

    test: Write tests

--- test/mineral_mongodb_test.dart
+++ test/mineral_mongodb_test.dart
@@ -1,8 +1,54 @@
+import 'package:mineral_ioc/ioc.dart';
 import 'package:mineral_mongodb/mineral_mongodb.dart';
+import 'package:mineral_mongodb/src/entities/schema.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 import 'package:test/test.dart';
 
+class Foo extends Schema<Foo> {
+  late String bar;
+}
+
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  final mongodb = MongoDB()
+    ..local(Uri(
+      host: '127.0.0.1',
+      port: 27017,
+      scheme: 'mongodb',
+      path: 'mongodb',
+    ));
+
+  test('can registered into mineral ioc', () {
+    ioc.bind(namespace: MongoDB.namespace, service: mongodb);
+    expect(ioc.singleton(MongoDB.namespace), equals(mongodb));
+  });
+
+  test('can connect to database', () async {
+    await mongodb.open();
+    expect(mongodb.connection.database.state, equals(State.OPEN));
+  });
+
+  test('can get all', () async {
+    final foo = await Schema.all<Foo>();
+    expect(foo.length, equals(0));
+  });
+
+  test('can create one element', () async {
+    final foo = await Schema.create<Foo>((schema) {
+        schema.bar = 'bar';
+    });
+
+    final result = await Schema.all<Foo>();
+
+    expect(foo.runtimeType, equals(Foo));
+    expect(result.length, equals(1));
+  });
+
+  test('can create many elements', () async {
+    final tests = await Schema.createMany<Foo>([
+      (schema) => schema.bar = 'Bar1',
+      (schema) => schema.bar = 'Bar2'
+    ]);
+
+    expect(tests.length, equals(2));
   });
 }
