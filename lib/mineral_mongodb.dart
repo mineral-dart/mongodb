2c324a2bd93946a0d90e6c73a3b8f3fda1c4738c
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2022-12-15 10:27:27 +0100

    feat: Export members

--- lib/mineral_mongodb.dart
+++ lib/mineral_mongodb.dart
@@ -1,3 +1,5 @@
+export 'package:mongo_dart/mongo_dart.dart';
+
 export 'src/mongodb.dart' show MongoDB;
 export 'src/entities/schema.dart' show Schema;
-export 'package:mongo_dart/mongo_dart.dart';
+export 'src/mixins/transaction.dart' show Transaction;
